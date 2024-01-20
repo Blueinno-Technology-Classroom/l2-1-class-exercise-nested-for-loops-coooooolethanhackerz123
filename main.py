4f36eb4e1c54a057f39d19f684c340ece72bd6f0
cooooooolethanhackerz22113 <wangethanhk@gmail.com>
2024-01-20 11:59:34 +0800

    h

--- main.py
+++ main.py
@@ -4,34 +4,52 @@ Q1:
 '''
 
 # TODO: Write your code here
-
+for j in range(8):
+    for i in range(j):
+        print(i, end=" ")
+    print()
 ##################################################
 '''
 Q2:
 '''
 
 # TODO: Write your code here
-
+for j in range(1,8):
+    for i in range(1,j):
+        print(i, end=' ')
+    print()
 ##################################################
 '''
 Q3:
 '''
 
 # TODO: Write your code here
-
+for j in range(9,0,-1):
+    for i in range(5):
+        print(j, end='')
+print()
 ##################################################
 '''
 Q4:
 '''
 
 # TODO: Write your code here
+for j in range(9,0,-1):
+    for i in range(j):
+        print(j, end='')
 
+print()
 ##################################################
 '''
 Q5:
 '''
 
 # TODO: Write your code here
+for j in range(1,10,2):
+    print('-' * (5-int(j/2)), end='')
+    for i in range(j):
+        print(j, end='')
+    print()
 
 ##################################################
 '''
@@ -39,5 +57,12 @@ Q6:
 '''
 
 # TODO: Write your code here
+for i in range(1,6):
+    print(' ' * (5-i+1), end='')
+    for j in range(i):
+        print(j+1, end='')
+    for j in range(i-1,0,-1):
+        print(j, end='')
+    print()
 
 ##################################################
